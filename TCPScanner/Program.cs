63b7017ebf014abf2d484a27e74009ed99e639a0
Max <Losckutow.maxim@yandex.ru>
2020-04-16 21:24:22 +0600

    TCPScanner task

--- TCPScanner/Program.cs
+++ TCPScanner/Program.cs
@@ -10,11 +10,13 @@ namespace TCPScanner
         {
             Console.WriteLine("Сканер портов TCP" + "\n" + "Выполнил Лоскутов Максим МО-202, МЕН-282203"
                 + "\n" + "Программа выводит список открытых портов в заданном диапазоне"
-                + "\n" + "Если открытых портов не надено, то программа ничего не выведете" + "\n");
+                + "\n" + "Если открытых портов не надено, то программа ничего не выведет" + "\n");
+
             Console.WriteLine("Введите начальный порт: ");
             var start = int.Parse(Console.ReadLine());
             Console.WriteLine("Введите конечный порт: ");
             var end = int.Parse(Console.ReadLine());
+
             if (start >= end) Console.WriteLine("Начальный порт должен быть меньше конечного");
             else
             {
@@ -26,7 +28,6 @@ namespace TCPScanner
                     {
                         Console.WriteLine("Port №" + i.ToString() + " is open");
                     }
-
                 }
             }
             Console.WriteLine("Для выхода нажмите любую клавишу...");
